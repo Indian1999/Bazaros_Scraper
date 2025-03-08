926a7474e923c73aca901b7e86aeafde1b7be331
Indian1999 <haszab99@gmail.com>
2025-03-08 17:25:00 +0100

    Basic product info scraped

--- bazaros_scrape.py
+++ bazaros_scrape.py
@@ -28,15 +28,28 @@ for result in results:
     product["price"] = result.find_element(By.CSS_SELECTOR, ".ty-price-num").text
     product["img-url"] = result.find_element(By.TAG_NAME, "img").get_attribute("src")
     product["url"] = result.find_element(By.CSS_SELECTOR, "a.product_icon_lnk").get_attribute("href")
-    product["price-per-unit"] = result.execute_script("""
-            return Array.from(document.querySelectorAll('span')).find(el => {
-                const style = window.getComputedStyle(el);
-                return style.color === 'rgb(255, 0, 0)' && 
-                       style.fontSize === '16px' &&
-                       style.fontWeight === '700' && 
-                       style.paddingTop === '8px';
-            });
-            """).text
+    
+    ppu_elem = driver.execute_script("return arguments[0].querySelectorAll('span');", result)
+    ppu = None
+    for elem in ppu_elem:
+        #style = driver.execute_script("return window.getComputedStyle(arguments[0]);", elem)
+        style = driver.execute_script("""
+    let elem = arguments[0]; 
+    return {
+        color: window.getComputedStyle(elem).getPropertyValue('color'),
+        fontSize: window.getComputedStyle(elem).getPropertyValue('font-size'),
+        fontWeight: window.getComputedStyle(elem).getPropertyValue('font-weight'),
+        paddingTop: window.getComputedStyle(elem).getPropertyValue('padding-top')
+    };
+""", elem)
+        print(style)
+        if (style["color"] == "rgb(255, 0, 0)" and 
+            style["fontSize"] == "16px" and
+            style["fontWeight"] == "700" and
+            style["paddingTop"]== "8px"):
+            ppu = elem.text
+            break
+    product["price-per-unit"] = ppu
     products.append(product)
 
 with open("products.json", "w", encoding="utf-8") as f:
